1dd512b57d50d31e58ec98492639013fbd2e8f02
asmapk <asma.k.patel1@gmail.com>
2021-10-31 16:07:35 +0100

    Timer part added

--- main.c
+++ main.c
@@ -11,20 +11,12 @@
 #define LED_PIN 2
 
 void main (void) {
-	DDRB |= (1 << LED_PIN);
-	uart_init();
+	DDRB |= (1 << LED_PIN); //set pin 
+
 	int TimeVariable = 0;
-	
-	
-    TCCR0A = (1<<WGM01 ) | (0<<WGM00);//select CTC mode Ca 19.9.1 page 138
-	TCCR0B = (0<<WGM02 ); //selecst CTC mode,normal opration, OC0A(PD6) is disconnected
-	
-	//TCCR0A = (1<<COM0A1); //Clear OC0A on Compare Match.
-	
-	TCCR0B |= (1<<CS02 ) | (0<<CS01 )|(1<<CS00 ) ;//setting the prescalar to 1024 ,this sets the clock,ATmel 42735 datasheet page 141 ,142
-  
-  
-    OCR0A = 156; // f_OCnx = f_clk_i/o / (2*N(1+OCRnx))  //    OCR0A = 16000000HZ/(2*1024*100HZ)
+
+	uart_init();
+	timer_init();
 
 
 	while (1){
